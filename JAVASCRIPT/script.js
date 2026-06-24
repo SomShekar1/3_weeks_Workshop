48d90910bd4432957557d31e59985d32ad0f215a
Somshekar B <rajsomshekar@gmail.com>
2026-06-24 14:51:36 +0530

    Javascript Day1, Its very Good And Interesting Language

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,13 @@
+let marks = 80
+if (marks >= 90){
+    console.log("Excellent")
+}
+else if(marks > 60 && marks < 90){
+    console.log("Above Average")
+}
+else if (marks <= 60 && marks > 40){
+    console.log("Average")
+}
+else{
+    console.log("Poor")
+}
---
+++ JAVASCRIPT/switch.js
@@ -0,0 +1,18 @@
+let day ="Mondy"
+switch (day) {
+
+    case "Monday":
+    case "Tuesday":
+    case "Wednesday":    
+    case "Thursday":
+    case "Friday":    
+        console.log("Weekdays");
+        break;
+
+    case "Sunday":
+    case "Saturday":
+        console.log("Weekend");
+        break;
+    default:
+        console.log("Sariyagi Enter Madu Day na")
+}
